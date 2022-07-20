7f56219530b93491b98403c6da52555135846d27
MDShihab235 <msub111244@gmail.com>
2022-07-20 18:13:02 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// typing animation
+const typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Developer.",
+    "Web Designer.",
+    "Graphic Designer.",
+    "Youtuber",
+  ],
+  typeSpeed: 100,
+  Backspeed: 60,
+  loop: true,
+});
